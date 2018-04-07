2d96f4c1b8c28f8bb746546baa4b5a453f139b11
nasdan <daniel.sanchez@lemoncode.net>
2018-04-07 21:23:02 +0200

    Update package.json file

--- src/updatePackageJson/updatePackageJson.js
+++ src/updatePackageJson/updatePackageJson.js
@@ -1,5 +1,31 @@
+const fs = require('fs');
+const { getProcessBasePath } = require('../helpers');
+const { scripts, jest } = require('./constants');
+
 module.exports = {
   updatePackageJson: async (config) => {
+    const basePath = getProcessBasePath();
+    const path = `${basePath}/package.json`;
+    const file = require(path);
 
+    file.scripts = {
+      ...file.scripts,
+      ...scripts,
+    };
+
+    if (config.useJest) {
+      file.jest = {
+        ...jest,
+      };
+    }
+    return await updatePackageJsonFile(file, path);
   }
 }
+
+const updatePackageJsonFile = (file, path) => new Promise((resolve, reject) => {
+  fs.writeFile(path, JSON.stringify(file), (error) => (
+    Boolean(error) ?
+      reject(error) :
+      resolve()
+  ));
+});
